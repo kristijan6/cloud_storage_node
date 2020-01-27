063f28877f6e712cd2bb8b4739bd5db3ad40f8c6
Kristijan Palic <kristijan.palic.96@gmail.com>
2020-01-28 00:50:22 +0100

    Initializing repo

---
+++ src/main/java/com/univie/cc/cloudStorageNode/CloudStorageNodeApplication.java
@@ -0,0 +1,13 @@
+package com.univie.cc.cloudStorageNode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStorageNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univie/cc/cloudStorageNode/CloudStorageNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univie.cc.cloudStorageNode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
